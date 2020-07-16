2b0deb8a64228d8eac71bf9f0fad967c9b4d99b3
ADAM <zebolsky.adam@gmail.com>
2020-07-16 17:30:32 -0500

    update resume section and add recipes carousel

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -4,33 +4,16 @@ import "./Home.css";
 const home = () => {
   return (
     <section id="home">
-      <div>
-        <div>
-          <h1>Cassie Brown</h1>
-          <h3>MS RD LD</h3>
-        </div>
+      <div className="section parallax bg1">
+        <h1>Cassie Brown</h1>
+        <h3>MS RD LD</h3>
         {/* <img src="https://i.imgur.com/N9dkVfg.jpg" alt="cassie & coffee" /> */}
       </div>
-      {/* <h4>
-        <img
-          src="https://i.imgur.com/bofb7de.png"
-          alt="cactus icon"
-          width="30px"
-          height="30px"
-        />{" "}
-        Dallas, TX
-      </h4> */}
-      <h4>Oh hey!</h4>
-      <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p>
-      <div className="picture-block">
-        {/* <img src="https://i.imgur.com/Box2HZp.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/uBPLK6e.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/Box2HZp.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/LnmLlbT.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/uBPLK6e.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/Box2HZp.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/LnmLlbT.jpg" alt="cassie & coffee" />
-        <img src="https://i.imgur.com/uBPLK6e.jpg" alt="cassie & coffee" /> */}
+      {/* <div className="section static">
+        <h4>Oh hey!</h4>
+      </div> */}
+      <div className="section parallax bg2">
+        <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p>
       </div>
     </section>
   );
--- src/Components/recipes/Recipes.js
+++ src/Components/recipes/Recipes.js
@@ -1,15 +1,56 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import "./Recipes.css";
 
+const recipeItems = [
+  {
+    title: "5-Spice Vegan Tacos",
+    url: "https://www.theppk.com/2020/05/bbq-5-spice-tempeh-mushroom-tacos/",
+    image: "https://i.imgur.com/tXr18WL.jpg",
+  },
+  {
+    title: "5-Spice Vegan Tacos",
+    url: "https://www.theppk.com/2020/05/bbq-5-spice-tempeh-mushroom-tacos/",
+    image: "https://i.imgur.com/LnmLlbT.jpg",
+  },
+  {
+    title: "5-Spice Vegan Tacos",
+    url: "https://www.theppk.com/2020/05/bbq-5-spice-tempeh-mushroom-tacos/",
+    image: "https://i.imgur.com/tXr18WL.jpg",
+  },
+];
+
 const recipes = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "slides",
+  };
   return (
-    <section id="recipes">
+    <section style={{ padding: "24px" }}>
       <h1>Recipes</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis,
-        blanditiis expedita? Earum eligendi pariatur quaerat quos expedita ab
-        quibusdam ratione veniam in, mollitia fuga repudiandae?
-      </p>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+      </Slider>
     </section>
   );
 };
