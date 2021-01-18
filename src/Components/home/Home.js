07c3197073cdbffe87670254ea1159813b7679a3
ADAM <zebolsky.adam@gmail.com>
2021-01-18 09:15:09 -0600

    update styles

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -3,32 +3,35 @@ import "./Home.css";
 
 const home = () => {
   return (
-    // <section id="home">
-    //   <h1>Cassie Brown</h1>
-    //   <h3>MS RD LD</h3>
-    //   <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p>
-    //   <div className="pic-container">
-    //     <div className="offset"></div>
-    //     <img
-    //       className="cassie-image"
-    //       src="https://i.imgur.com/N9dkVfg.jpg"
-    //       alt="cassie"
-    //     />
-    //   </div>
-    // </section>
-    <section id="home">
-      {/* <h1>Cassie Brown</h1>
-      <h3>MS RD LD</h3>
-      <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p> */}
-      <div className="pic-container">
-        <div className="offset"></div>
-        <img
-          className="cassie-image"
-          src="https://i.imgur.com/N9dkVfg.jpg"
-          alt="cassie"
-        />
-      </div>
-    </section>
+    <>
+      {/* <section id="home">
+        <h1>Cassie Brown</h1>
+        <h3>MS RD LD</h3>
+        <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p>
+        <div className="pic-container">
+          <div className="offset"></div>
+          <img
+            className="cassie-image"
+            src="https://i.imgur.com/N9dkVfg.jpg"
+            alt="cassie"
+          />
+        </div>
+      </section> */}
+      <section id="home">
+        <div className="home-heading">
+          <h1>Cassie Brown</h1>
+          <h3>MS RD LD</h3>
+          <p>Hey! I'm Cassie, a Registered Dietitian in Dallas, TX.</p>
+        </div>
+        <div className="pic-container">
+          <img
+            className="cassie-image"
+            src="https://i.imgur.com/N9dkVfg.jpg"
+            alt="cassie"
+          />
+        </div>
+      </section>
+    </>
   );
 };
 
